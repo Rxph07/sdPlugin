23e40f9deae335abced525db6d9a171935c4b982
Rxphe <raphael.weikl.hi@gmail.com>
2025-02-14 21:58:27 +0100

    Version 2.2.0-beta - Added a HotkeyInput

---
+++ sdPlugin/Input/HotkeyInput.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using WindowsInput.Native;
+
+namespace sdPlugin.Input
+{
+    public class HotkeyInput : BaseInput
+    {
+        public List<VirtualKeyCode> Modifiers { get; set; } = new List<VirtualKeyCode>();
+        public VirtualKeyCode Key { get; set; } = 0;
+        public HotkeyInput(string name) : base(name)
+        {
+        }
+    }
+}
